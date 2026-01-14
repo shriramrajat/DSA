26f5e830b50b292c76c2f60b7804a5d153db031d
shriramrajat <rvshriram@dpcoepune.edu.in>
2026-01-14 23:28:33 +0530

    modified:   Arrays/Medium/rearrange.cpp

--- Arrays/Medium/rearrange.cpp
+++ Arrays/Medium/rearrange.cpp
@@ -1,3 +1,20 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+int main(){
+      vector<int> nums = {1, 5, 3, -4, -6, -7};
+      int n = nums.size();
+      vector<int> arr(n);
+      int pos = 0;
+      int neg = 1;
+      for (int i = 0; i < n; i++){
+            if (nums[i] > 0){
+                  arr[pos] = nums[i];
+                  pos = pos + 2;
+            }
+            if (nums[i] < 0){
+                  arr[neg] = nums[i];
+                  neg = neg + 2;
+            }
+      }
+}
